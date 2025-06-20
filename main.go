67cdf30b506f58d954deada115dba6b4d7e207e4
Aziz Ridhwan Pratama <azizpratama2196@gmail.com>
2025-06-20 20:15:20 +0700

    app: Allow CORS
    Signed-off-by: Aziz Ridhwan Pratama <azizpratama2196@gmail.com>

--- main.go
+++ main.go
@@ -6,11 +6,28 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	// Set this to gin.DebugMode if you want to debug this program
 	gin.SetMode(gin.DebugMode)
 
 	r := gin.Default()
+	r.Use(CORSMiddleware())
 
 	paths := r.Group("/")
 	routes.APIRoutes(paths)
